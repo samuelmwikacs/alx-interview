33bd7048dd92ac570367d79dd5cb7b7fe0509cd7
samuel mwika <36988997+samuelmwikacs@users.noreply.github.com>
2023-02-03 23:18:41 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
-"""
-Contains a function called canUnlockAll
-"""
+"""Solution to Lockboxes problem"""
 
 
 def canUnlockAll(boxes):
-    """
-    Determines if all boxes can be opened
-    """
-    keys = set()
-
-    for box_id, box in enumerate(boxes):
-        if len(box) == 0 or box_id == 0:
-            keys.add(box_id)
-        for key in box:
-            if key < len(boxes) and key != box_id:
-                keys.add(key)
-        if len(keys) == len(boxes):
-            return True
-    return False
-
+    """Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
